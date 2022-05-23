2ca303c02b6684f39c18c937cb011f4afea1316f
Emmanuel Hayford <hi@manny.codes>
2022-05-23 22:58:46 +0200

    Fix syntax error

--- app/jobs/scrape_events_job.rb
+++ app/jobs/scrape_events_job.rb
@@ -8,13 +8,14 @@ class ScrapeEventsJob < ApplicationJob
     # TODO: Could use some refactoring
     events.each do |event|
       Event.create_with(
-        start_date: Date.parse(DateSplitter.parse(event[:date]).join(",").split(",").first),
-        end_date: Date.parse(DateSplitter.parse(event[:date]).join(",").split(",").last),
+        start_date: Date.parse(DateSplitter.parse(event[:date]).join(',').split(',').first),
+        end_date: Date.parse(DateSplitter.parse(event[:date]).join(',').split(',').last),
         venue: event[:location],
         twitter_handle: event[:twitter_handle]
       ).find_or_create_by(name: event[:name])
     end
   end
+end
 
 # Scraper::RubyConferences.scraped_events.map do |ary|
 #   Scraper::Transformer.new(ary).hashify
